e123963bd51a7579dfd76beafc3557f3983e3f45
Ibrahim-Sikder <ibrahimsikder5033@gmail.com>
2023-12-02 11:04:06 +0600

    trust-auto-solution-project-setup

--- index.js
+++ index.js
@@ -8,37 +8,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fomplst.mongodb.net/?retryWrites=true&w=majority`;
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
-
-async function run() {
-  try {
-    await client.connect();
-    const serviceCollection = client.db("TrusAutoSolution").collection("services");
-
- 
-
-
-
-
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      " You successfully connected to MongoDB!"
-    );
-  } finally {
-  }
-}
-run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Trust Auto Solution ");
